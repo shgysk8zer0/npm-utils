f12e6eac9f941da0998ac9b661aec48682e6860e
Chris Zuber <shgysk8zer0@gmail.com>
2023-10-04 11:33:35 -0700

    Update getting URL from paths

--- url.js
+++ url.js
@@ -2,12 +2,12 @@ import { ROOT } from './consts.js';
 import { isPath, isURL } from './utils.js';
 import { getFileURL } from './path.js';
 
-export function pathToURL(path, base = ROOT) {
+export function pathToURL(path, base = ROOT.pathname) {
 	if (path instanceof URL) {
 		return path;
 	} else if (isPath(base)) {
 		// Absolute paths should be relative to project root/base
-		return getFileURL(path, base);
+		return getFileURL(path.startsWith('/') ? `.${path}` : path, base);
 		/*return new URL(
 			path.startsWith('/') ? `.${path}` : path,
 			`file://${base.replace('file://', '')}`
