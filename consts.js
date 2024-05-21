0f7d7e83d1c7fd3232d4a4cc5f28179fe040e36e
Chris Zuber <shgysk8zer0@gmail.com>
2024-05-21 14:09:31 -0700

    Misc updates and fixes
    - Handle all cases for `path` when writing files
    - Resolve weird issue when `import * as FOO` is used in building with esbuild

--- consts.js
+++ consts.js
@@ -4,7 +4,10 @@ import { pathToFileURL } from 'node:url';
 export const LF = '\n';
 export const CRLF = '\r\n';
 export const TAB = '\t';
-export const URL_PREFIXES = ['https:', 'http:', 'blob:', 'data:'];
+export const WEB_PREFIXES = ['https:', 'http:', 'blob:', 'data:', 'ws:', 'javascript:'];
+export const URL_PREFIXES = WEB_PREFIXES;
+export const OTHER_PPEFIXES = ['tel:', 'mailto:', 'geo:', 'magnet:'];
+export const FTP_PREFIXES = ['git:', 'ssh:', 'ftp:', 'ftps:', 'sftp:', 'dav:', 'davs:'];
 export const PATH_PREFIXES = ['file:', '/', './', '../'];
 export const ROOT = 'process' in globalThis && process.cwd instanceof Function
 	? pathToFileURL(`${process.cwd()}/`)
